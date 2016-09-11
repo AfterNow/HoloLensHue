9086335eec07a3b551eee1088b8eddafdde31584
AnthonyCorrado <amcstudios@yahoo.com>
2016-09-11 00:33:23 -0700

    adds app state and hue api error handling

--- Assets/Scripts/HueBridgeManager.cs
+++ Assets/Scripts/HueBridgeManager.cs
@@ -28,69 +28,89 @@ public class HueBridgeManager : MonoBehaviour {
     void Start()
     {
        // MOCK smart lights for testing
-       mockLights = new MockSmartLights();
-       smartLights = mockLights.getLights();
-       convertLightData();
+       //mockLights = new MockSmartLights();
+       //smartLights = mockLights.getLights();
+       //convertLightData();
        // MOCK end mock setup
 
         if ((!bridgeip.Equals("127.0.0.1")) && (!username.Equals("newdeveloper")))
         {
-            Debug.Log("co called");
-            StartCoroutine(DiscoverLights(convertLightData));
+            if (StateManager.Instance.CurrentState == StateManager.HueAppState.Start)
+            {
+                StateManager.Instance.CurrentState = StateManager.HueAppState.ConnectedDevices_Initializing;
+                StartCoroutine(DiscoverLights(convertLightData));
+            }
+            else
+            {
+                Debug.Log("There was an error with the app startup state");
+            }
+        }
+        else
+        {
+            Debug.Log("Please enter your Bridge IP and username");
         }
     }
 
     public IEnumerator DiscoverLights(Action nextAction)
     {
-        lights_json = UnityWebRequest.Get("http://" + bridgeip + "/api/" + username + "/lights");
-        Debug.Log("Hue Response Errors: " + lights_json.error);
+        string url = "http://" + bridgeip + "/api/" + username + "/lights";
+        Debug.Log("Request url: " + url);
+
+        lights_json = UnityWebRequest.Get(url);
+        if (lights_json.error != null)
+        {
+            Debug.Log("There was an error. Your request was not sent");
+        }
         yield return lights_json.Send();
 
-        nextAction();
-
-        Debug.Log("http" + bridgeip + portNumber + "/api/" + username + "/lights");
-    }
-
-    private List<SmartLight> getAllLights()
-    {
-        Debug.Log("getAll called");
-        return smartLights;
-    }
-
-    public List<SmartLight> GetLightCollection()
-    {
-        return smartLights;
+        string jsonValue = lights_json.downloadHandler.text;
+        if (jsonValue.Contains("error"))
+        {
+            Debug.LogError("A bridge could not be found or could not be accessed at this time.");
+            StateManager.Instance.CurrentState = StateManager.HueAppState.ConnectedDevices_Failed;
+        }
+        else
+        {
+            Debug.Log(lights_json.downloadHandler.text);
+            StateManager.Instance.CurrentState = StateManager.HueAppState.Ready;
+            nextAction();
+        }
     }
 
     void convertLightData()
     {
-        var lights = (Dictionary<string, object>)Json.Deserialize(lights_json.downloadHandler.text);
-        foreach (string key in lights.Keys)
+        if (StateManager.Instance.CurrentState == StateManager.HueAppState.Ready && lights_json != null)
         {
-            // init state types
-            bool on;
-            int id, bri, hue, sat;
-            string effect, alert;
+            var lights = (Dictionary<string, object>)Json.Deserialize(lights_json.downloadHandler.text);
+            foreach (string key in lights.Keys)
+            {
+                // init state types
+                bool on;
+                int id, bri, hue, sat;
+                string effect, alert;
 
-            //Debug.Log("made it to the foreach loop "+ key);
+                var light = (Dictionary<string, object>)lights[key];
+                var state = (Dictionary<string, dynamic>)light["state"];
 
-            var light = (Dictionary<string, object>)lights[key];
-            var state = (Dictionary<string, dynamic>)light["state"];
+                // converting needs to be done prior to instantiating new SmartLight
+                on = Convert.ToBoolean(state["on"]);
+                bri = Convert.ToInt32(state["bri"]);
+                hue = Convert.ToInt32(state["hue"]);
+                sat = Convert.ToInt32(state["sat"]);
+                effect = Convert.ToString(state["effect"]);
+                alert = Convert.ToString(state["alert"]);
 
-            // converting needs to be done prior to instantiating new SmartLightState
-            on = Convert.ToBoolean(state["on"]);
-            bri = Convert.ToInt32(state["bri"]);
-            hue = Convert.ToInt32(state["hue"]);
-            sat = Convert.ToInt32(state["sat"]);
-            effect = Convert.ToString(state["effect"]);
-            alert = Convert.ToString(state["alert"]);
+                id = Convert.ToInt32(key);
 
-            id = Convert.ToInt32(key);
-
-            State smartLightState = new State(on, bri, hue, sat, effect, alert);
-            smartLights.Add(new SmartLight(id, light["name"].ToString(), light["modelid"].ToString(), smartLightState));
+                State smartLightState = new State(on, bri, hue, sat, effect, alert);
+                smartLights.Add(new SmartLight(id, light["name"].ToString(), light["modelid"].ToString(), smartLightState));
+            }
+            SendMessage("createLights", smartLights);
         }
-        SendMessage("createLights", smartLights);
+        else
+        {
+            Debug.LogError("No lights can be found. Please ensure the Bridge IP is set and your lighting system is functioning properly");
+        }
     }
 
     public void TestPut()
---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using HoloToolkit.Unity;
+
+/// <summary>
+/// Manages the current state of the application.
+/// </summary>
+public class StateManager : Singleton<StateManager>
+{
+    [Tooltip("Current state of the application")]
+    public string appState;
+
+    public enum HueAppState
+    {
+        // Overall states
+        Start,
+        Ready,
+        Editing,
+        // IoT Devices values
+        ConnectedDevices_Initializing,
+        ConnectedDevices_Initialized,
+        ConnectedDevices_Failed
+    }
+
+    HueAppState currentState = HueAppState.Start;
+
+    public HueAppState CurrentState
+    {
+        get
+        {
+            return currentState;
+        }
+        set
+        {
+            currentState = value;
+            appState = CurrentStateName;
+        }
+    }
+
+    public string CurrentStateName
+    {
+        get
+        {
+            return currentState.ToString();
+        }
+    }
+
+    public bool SearchingForDevices
+    {
+        get
+        {
+            return currentState == HueAppState.ConnectedDevices_Initialized;
+        }
+    }
+
+    public bool DevicesFound
+    {
+        get
+        {
+            return currentState == HueAppState.Ready;
+        }
+    }
+
+    void Start()
+    {
+        appState = CurrentStateName;
+    }
+}
