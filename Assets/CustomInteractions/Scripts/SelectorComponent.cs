989a3e76b2645cd318e5a9fd2377fb079f151a78
AnthonyCorrado <amcstudios@yahoo.com>
2016-11-11 13:08:20 -0800

    adds color wheel and starts selector functionality

---
+++ Assets/CustomInteractions/Scripts/SelectorComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorComponent : MonoBehaviour {
+
+    GameObject colorWheel;
+
+	// Use this for initialization
+	void Start () {
+        colorWheel = GameObject.Find("RainbowWheel");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(colorWheel.transform);
+        Debug.Log("selector pos" + transform.position);
+	}
+
+    void LateUpdate()
+    {
+        performRayCast();
+    }
+
+    void performRayCast()
+    {
+        var rayDirection = gameObject;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo,
+            0.2f))
+        {
+            Debug.Log("hit info: " + hitInfo.collider.name);
+        }
+    }
+}
