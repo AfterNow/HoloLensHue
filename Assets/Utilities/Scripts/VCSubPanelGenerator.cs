30034e4dbb4a7e15e41d4461d8477eaead2f037e
AnthonyCorrado <amcstudios@yahoo.com>
2017-01-18 15:36:17 -0800

    adds dynamic population of keyPhrases and actions fields

--- Assets/Utilities/Scripts/VCSubPanelGenerator.cs
+++ Assets/Utilities/Scripts/VCSubPanelGenerator.cs
@@ -24,14 +24,27 @@ public class VCSubPanelGenerator : MonoBehaviour {
 
         foreach (VCSubModule module in subModules)
         {
-            var moduleObject = Instantiate(subModulePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            var moduleObject = Instantiate(subModulePrefab, transform.localPosition, Quaternion.identity);
             moduleObject.name = module.KeyPhrase;
 
             // gets newly instantiated GameObject and sets to child of Parent GameObject 
-            GameObject currentModule = GameObject.Find(module.KeyPhrase);
+            GameObject currentModule = GameObject.Find(moduleObject.name);
             currentModule.transform.parent = gameObject.transform;
+
+            // sets the proper scale and placement of submodules
+            RectTransform rect = currentModule.GetComponent<RectTransform>();
+            rect.localPosition = new Vector3(0, 0, 0);
+            rect.localScale = new Vector3(1, 1, 1);
+
+            // sets the keyPhrase in the proper text field to be displayed
+            var keyPhraseText = currentModule.transform.GetChild(0).GetChild(0).gameObject;
+            keyPhraseText.GetComponent<Text>().text = module.KeyPhrase;
+
+            // sets the action in the proper text field to be displayed
+            var actionText = currentModule.transform.GetChild(1).GetChild(0).gameObject;
+            actionText.GetComponent<Text>().text = module.Action;
         }
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
